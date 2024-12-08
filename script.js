50c7db794669e9062a596b17e018da4302c772c7
0022491805 <marziehmonshizade@yahoo.com>
2024-12-08 15:18:13 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// const submitBtn = document.getElementsByClassName("submit-btn");
+// submitBtn.addEventListener("click", () => {
+//   console.log("click");
+// });
+function hideShowItem(val) {
+  if (val == 1) {
+    document.getElementById("test").style.display = "none";
+  } else {
+    // document.getElementById("test").addEventListener.apply("");
+  }
+}
+// const input = document.getElementById("cash");
+// purchaseBtn.addEventListener("click", () => {
+//   console.log(input.value);
+// });
